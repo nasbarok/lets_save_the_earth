3b86e321c1a3331168fc667d6295d2855b97a1a8
nasbarok <na.ouahit@gmail.com>
2025-09-23 01:30:03 +0200

    start

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.letsavetheearth.game',
+  appName: "Let's Save the Earth",
+  webDir: 'dist'  // Changé de 'www' vers 'dist'
+};
+
+export default config;
+
